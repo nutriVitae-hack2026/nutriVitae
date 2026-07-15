3836b42520319935e4eeb5ecb644c064ca016646
saraSchulz-art <sara.ifc2024@gmail.com>
2026-07-14 21:32:04 -0300

    header e footer criação do header e importação para app vue
    import material icons mdi
    add logo na parte de cima do site
    criação de bse e main css
    imposrtação da fonte "italiana"

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import Header from './components/headerComponent.vue'
+import Footer from './components/footerComponent.vue'
+</script>
 
 <template>
+  <Header />
   <router-view />
+  <Footer />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@mdi/font/css/materialdesignicons.min.css'
+import './assets/main.css'
 
 const app = createApp(App)
 
