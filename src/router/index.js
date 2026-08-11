ed122f77836acd1c2b0a80e23dab091452e8d453
mizaelestevam <vambastter14@gmail.com>
2026-08-11 13:53:17 -0300

    feat: adicionando cadastro com a cofiguração das rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import cadastroComponents from '../components/suporte/cadastroCoponents.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'cadastroComponents',
+      component: cadastroComponents,
+    }
+  ],
 })
 
 export default router
