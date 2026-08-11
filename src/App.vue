6785963540bf62c034c89fda94c467e4c8250ffa
mizaelestevam <vambastter14@gmail.com>
2026-08-11 16:52:48 -0300

    feat: implementar componente de cadastro com campos de formulário e remover configuração de rotas

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import Header from './components/headerComponent.vue'
 import Footer from './components/footerComponent.vue'
+import cadastroCoponents from './components/suporte/cadastroCoponents.vue';
 </script>
 
 <template>
   <Header />
-    <router-view />
+    <!-- <router-view /> -->
+     
+    <cadastroCoponents/>
   <Footer />
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import cadastroComponents from '../components/suporte/cadastroCoponents.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'cadastroComponents',
-      component: cadastroComponents,
-    }
-  ],
+  
 })
 
 export default router
