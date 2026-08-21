e47eff53ad0cc63f9c2c05296a5cffc3b234a864
Bianca161 <biancaholandadejesus>
2026-08-21 14:08:03 -0300

    feat: adicionar rota para cadastro de pratos e criar componente de busca

---
+++ src/components/pratos/buscarComponents.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  
+    <h1>Buscar Pratos</h1>
+    
+</template> 
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import cadastroComponents from '../components/pratos/cadastroComponents.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+   {
+            path: '/',
+            name: 'cadastroComponents',
+            component: cadastroComponents
+        },
+  ],
 })
 
 export default router
