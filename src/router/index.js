ed268b3cb569722e60c6463b68c7e5068f59b046
angelof612 <anjuanimayt@gmail.com>
2026-08-25 12:44:04 -0300

    feat: registra rota /academico e ajusta imports da aplicacao

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AcademicPage from '../views/AcademicPage.vue'
+
+const routes = [
+  {
+    path: '/academico',
+    name: 'Academic',
+    component: AcademicPage
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
 })
 
 export default router
