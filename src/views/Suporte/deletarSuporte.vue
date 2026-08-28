87e718069666feb655bb7b72d475b8ad279ce0bc
mizaelestevam <vambastter14@gmail.com>
2026-08-28 11:02:49 -0300

    feat: criar componente de perfil de suporte com formulário de entrada

---
+++ src/views/Suporte/deletarSuporte.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+
+const perfil = ref({
+  buscar: {
+    chamado: '',
+    status: '',
+    data: '',
+    solicitante: '',
+    prioridade: '',
+    categoria: '',
+    descricao: ''
+  },
+})
+</script>
+
+<template>
+  <div class="principal">
+  <h1>Perfil do suporte</h1>
+
+<div class="nome cartao-entrada">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" v-model="perfil.buscar.solicitante" />
+    </div>
+
+    </div>
+</template>
+
+<style scoped></style>
